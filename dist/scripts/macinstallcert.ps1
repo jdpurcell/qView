fefb270581ceed97347b71068cc884db7d723bbe
J.D. Purcell <jdpurcell@gmail.com>
2026-03-15 20:58:08 -0400

    CI: macOS signing cleanup, part 2

--- dist/scripts/macinstallcert.ps1
+++ dist/scripts/macinstallcert.ps1
@@ -12,10 +12,16 @@ $keychainPass = [Guid]::NewGuid().ToString()
 
 [IO.File]::WriteAllBytes($certPath, [Convert]::FromBase64String($env:APPLE_DEVID_APP_CERT_DATA))
 
+# Create a keychain
 & security create-keychain -p $keychainPass $keychainPath
+# Set it as default
 & security default-keychain -s $keychainPath
-& security unlock-keychain -p $keychainPass $keychainPath
-& security import $certPath -P $env:APPLE_DEVID_APP_CERT_PASS -T /usr/bin/codesign -k $keychainPath
+# Unlock it
+& security unlock-keychain -p $keychainPass
+# Clear settings to prevent locking after timeout
+& security set-keychain-settings
+# Import certificate and allow codesign to access it
+& security import $certPath -P $env:APPLE_DEVID_APP_CERT_PASS -T /usr/bin/codesign
 
 $cert = New-Object X509Certificate2($certPath, $env:APPLE_DEVID_APP_CERT_PASS)
 $certName = $cert.GetNameInfo([X509NameType]::SimpleName, $false)
